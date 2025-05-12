478f06f826de2fa2272b2b1101a2b4f86ae89c95
pavan0628 <pavan.cse.rymec@gmail.com>
2025-05-12 15:08:09 +0530

    [PAVAN]CREATE:Creating linked list and adding elements

--- src/LinkedListMainClass.java
+++ src/LinkedListMainClass.java
@@ -4,5 +4,12 @@ public class LinkedListMainClass {
     public static void main(String[] args) {
         System.out.println("Welecome to DSA problems");
 
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+
+        System.out.println(list);
+
     }
 }
